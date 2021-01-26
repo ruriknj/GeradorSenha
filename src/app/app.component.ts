4a4272444e077347db55a6ed889ccd0d788703ac
ruriknj <ruriknj@hotmail.com>
2021-01-26 17:33:48 -0300

    App - Gerador de senha

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pw';
+  length = 0;
+  includeLetters = false;
+  includeNumbers = false;
+  includeSymbols = false;
+  password = '';
+
+  onChangeLength(value: string) {
+    const parsedValue = parseInt(value);
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    } else {
+      alert("Digite um numero");
+    } 
+    
+  }
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+
+  onChangeSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+  
+  onButtonClick() {
+    const numbers = '1234567890';
+    const letters = 'abcdefghijlmnopqrstuvwyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatePassword = '';
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatePassword += validChars[index];
+    }
+
+    this.password = generatePassword;
+
+  }
+  
 }
